e48e860a4ad08f6dcba5964c5a6a62ec4dfd24c3
Vadym Balabukha <vadim.b@2muchcoffee.com>
2023-02-06 14:19:11 +0200

    feat(src): add typeorm & swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrmConfigService } from './shared/services/orm-config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, expandVariables: true }),
+    TypeOrmModule.forRootAsync({
+      useClass: OrmConfigService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
